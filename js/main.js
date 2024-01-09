c208d4e8acd51c53ac6dc94a8540258c22103103
Atila Fonseca <eng.atila.sete@gmail.com>
2024-01-09 17:38:45 -0300

    atividade com jQuery concluída

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    function inserirTarefaLista(){
+        const nome_tarefa = $('#nome_tarefa').val();
+        $(`<li>${nome_tarefa}</li>`).appendTo('ul');
+        $('#nome_tarefa').val('');
+    }
+    function tarefaExecutada(){
+        $('ul li').click( function(){
+            $(this).css("text-decoration","line-through");
+        });
+    }
+    
+    $('form').on('submit', function(e){
+        inserirTarefaLista();
+        tarefaExecutada();
+        
+    });
+    
+});
